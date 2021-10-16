777628646d06dfd0ef10fae61eb33fcf89709f83
jennifertakagi <jennitakagi@gmail.com>
2021-10-16 18:13:53 -0300

    feat: create authentication context and use it

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
+import { FormEvent, useState } from 'react';
+import styles from '../styles/Home.module.css';
+import { useAuth } from '../contexts/AuthContext';
+
 export default function Home() {
+  const { signIn } = useAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      email,
+      password,
+    }
+
+    await signIn(data);
+  }
+
   return (
-    <div>
-      Next Auth
-    </div>
+    <form onSubmit={handleSubmit} className={styles.container}>
+      <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+      <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      <button type="submit">Login</button>
+
+    </form>
   )
 }
